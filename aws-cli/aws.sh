e88678c670c9829678a93f8509ba4e20dbfae4aa
abdennour <mail@abdennoor.com>
2018-07-03 22:16:09 +0300

    aws cli docker image

---
+++ aws-cli/aws.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Put it in
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# enable interruption signal handling
+trap - INT TERM
+
+REGION=${AWS_DEFAULT_REGION:-us-east-1}
+
+docker run --rm \
+	-t $(tty &>/dev/null && echo "-i") \
+	-e "AWS_PROFILE=${AWS_PROFILE}" \
+	-e "AWS_DEFAULT_REGION=${REGION}" \
+	-v $(pwd):/project \
+  -v ~/.aws:/root/.aws \
+	docker-factory-aws-cli:1.15.50 \
+	"$@"
